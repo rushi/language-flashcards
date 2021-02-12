383567c07223acff63d4163463261e78a778672d
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-12 20:21:44 +0530

    Add styling for the last paragraph

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Page from '../components/Page';
 const Index = () => {
     return (
         <Page title="Learn Kannada">
-            <p className="">
+            <p>
                 How it works:
                 <ol className="ml-5 list-disc">
                     <li>Select a category</li>
                     <li>Tap on a card to reveal it's translation</li>
                 </ol>
             </p>
-            <p className="py-5">
+            <p className="py-5 text-gray-500">
                 More learning methods and little bit of design coming
             </p>
         </Page>
