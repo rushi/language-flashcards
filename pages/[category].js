cca123e78461db2efabed2436b275239673084cc
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-12 21:41:22 +0530

    Fix DOM nesting and dont shuffle due to error

--- pages/[category].js
+++ pages/[category].js
@@ -5,9 +5,8 @@ import Card from '../components/Card';
 
 const Grid = ({ category }) => {
     const list = words[category];
-    const cards = _.shuffle(_.keys(list)).map((source) => (
-        <Card key={source} source={source} destination={list[source]} />
-    ));
+    let cards = _.keys(list).map((source) => <Card key={source} source={source} destination={list[source]} />);
+    // cards = _.shuffle(cards); // Causes a console error 'Text content did not match. Server: "<foo>" Client: "<bar>"'
     return (
         <Page title={`${_.capitalize(category)} words`}>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">{cards}</div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Page from '../components/Page';
 const Index = () => {
     return (
         <Page title="Learn Kannada">
-            <p>
+            <div>
                 How it works:
                 <ol className="ml-5 list-disc">
                     <li>Select a category</li>
                     <li>Tap on a card to reveal it's translation</li>
                 </ol>
-            </p>
+            </div>
             <p className="py-5 text-gray-500">
                 More learning methods and little bit of design coming
             </p>
