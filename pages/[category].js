ea27ba9c36c6e01ec5a41520dd4196940fcb9e3c
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-13 10:34:28 +0530

    Shuffle words based on env var

--- pages/[category].js
+++ pages/[category].js
@@ -6,7 +6,7 @@ import Card from '../components/Card';
 const Grid = ({ category }) => {
     const list = words[category];
     let cards = _.keys(list).map((source) => <Card key={source} source={source} destination={list[source]} />);
-    cards = _.shuffle(cards);
+    cards = process.env.SHUFFLE_WORDS ? _.shuffle(cards) : cards;
     return (
         <Page title={`${_.capitalize(category)} words`}>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">
