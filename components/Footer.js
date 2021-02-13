5be8d7b60ab691e46d95a03ce10cfd8e7bac1668
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-13 14:29:14 +0530

    Updated footer + favicon

---
+++ components/Footer.js
@@ -0,0 +1,50 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+                <div className="flex justify-center space-x-4 md:order-2">
+                    <a
+                        target="_blank"
+                        title="Give feedback or fork this repo on Github"
+                        href="https://github.com/rushi/language-flashcards"
+                        className="text-gray-400 hover:text-gray-500"
+                    >
+                        <span className="sr-only">GitHub</span>
+                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path
+                                fillRule="evenodd"
+                                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </a>
+                    <a
+                        target="_blank"
+                        title="Find new words on Google Translate. They may not be accurate though"
+                        href="https://github.com/rushi/language-flashcards"
+                        className="text-gray-400 hover:text-gray-500"
+                    >
+                        <span className="sr-only">Google Translate</span>
+                        <svg className="h-6 w-6" viewBox="0 0 22 22" fill="currentColor">
+                            <path
+                                fillRule="evenodd"
+                                d="M7 2a1 1 0 011 1v1h3a1 1 0 110 2H9.578a18.87 18.87 0 01-1.724 4.78c.29.354.596.696.914 1.026a1 1 0 11-1.44 1.389c-.188-.196-.373-.396-.554-.6a19.098 19.098 0 01-3.107 3.567 1 1 0 01-1.334-1.49 17.087 17.087 0 003.13-3.733 18.992 18.992 0 01-1.487-2.494 1 1 0 111.79-.89c.234.47.489.928.764 1.372.417-.934.752-1.913.997-2.927H3a1 1 0 110-2h3V3a1 1 0 011-1zm6 6a1 1 0 01.894.553l2.991 5.982a.869.869 0 01.02.037l.99 1.98a1 1 0 11-1.79.895L15.383 16h-4.764l-.724 1.447a1 1 0 11-1.788-.894l.99-1.98.019-.038 2.99-5.982A1 1 0 0113 8zm-1.382 6h2.764L13 11.236 11.618 14z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </a>
+                </div>
+                <div className="mt-8 md:mt-0 md:order-1">
+                    <p className="text-center text-base text-gray-400">
+                        A learning project by{' '}
+                        <a target="_blank" className="underline" href="http://rushi.vishavadia.com">
+                            Rushi Vishavadia
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
