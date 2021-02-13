d3d44ddf9c8973cf5803fd388dceaeed08dce6be
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-13 17:20:30 +0530

    Use a serif font to handle words like "Illi" that can cause confusion

--- components/Card.js
+++ components/Card.js
@@ -7,11 +7,11 @@ const Card = (props) => {
 
     // TODO: better animation animate__animated animate__fadeInUpBig
     return (
-        <div onClick={show} className="bg-white rounded-sm mb-5 border-2 border-blue-200 cursor-pointer">
+        <div onClick={show} className="card bg-white rounded-sm mb-5 border-2 border-blue-200 cursor-pointer">
             <div className="w-full flex items-center justify-between p-6 space-x-6">
                 <div className="flex-1 truncate">
                     <div className="flex items-center space-x-3">
-                        <h3 className="source text-gray-900 text-md font-medium truncate">{props.source}</h3>
+                        <h3 className="source text-gray-900 text-md truncate">{props.source}</h3>
                     </div>
                     <p
                         className={`animate__animated ${visibility} whitespace-normal mt-1 text-green-400 text-sm truncate`}
--- components/Grid.js
+++ components/Grid.js
@@ -1,17 +1,11 @@
 import _ from 'lodash';
 import Card from './Card';
-import { ray } from 'node-ray/web';
-import { useState, useEffect } from 'react';
 
 const Grid = (props) => {
     const list = props.words;
     const cards = _.keys(list).map((source) => <Card key={source} source={source} destination={list[source]} />);
 
-    return (
-        <div className="">
-            <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">{cards}</div>
-        </div>
-    );
+    return <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">{cards}</div>;
 };
 
 export default Grid;
