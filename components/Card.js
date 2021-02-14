8997f0f4096a536d84f5dd8531864cac512cc89b
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-14 18:43:32 +0530

    Spacing

--- components/Card.js
+++ components/Card.js
@@ -10,12 +10,12 @@ const Card = (props) => {
         <div onClick={show} className="card bg-white rounded-sm mb-0 border border-blue-200 cursor-pointer">
             <div className="w-full flex items-center justify-between space-x-6">
                 <div className="flex-1 truncate">
-                    <div className="flex items-center space-x-3 bg-blue-100 border-b border-blue-200 px-3 py-1">
+                    <div className="flex items-center space-x-3 bg-blue-100 border-b border-blue-200 px-3 py-1.5">
                         <p className="source text-gray-900 text-sm">{props.source}</p>
                     </div>
                     <p
                         className={`animate__animated animate__faster ${visibility} whitespace-normal mt-1
-                        text-gray-800 text-md px-3 py-1 truncate`}
+                        text-gray-800 text-md px-3 py-1.5 truncate`}
                     >
                         {props.destination}
                     </p>
