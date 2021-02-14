a53e10cbbabb98b5d0b5d787a75a61d4cb40c5cc
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-14 12:17:36 +0530

    Some random styling

--- components/Card.js
+++ components/Card.js
@@ -7,14 +7,15 @@ const Card = (props) => {
 
     // TODO: better animation animate__animated animate__fadeInUpBig
     return (
-        <div onClick={show} className="card bg-white rounded-sm mb-5 border-2 border-blue-200 cursor-pointer">
-            <div className="w-full flex items-center justify-between p-6 space-x-6">
+        <div onClick={show} className="card bg-white rounded-sm mb-0 border border-blue-200 cursor-pointer">
+            <div className="w-full flex items-center justify-between space-x-6">
                 <div className="flex-1 truncate">
-                    <div className="flex items-center space-x-3">
-                        <h3 className="source text-gray-900 text-md truncate">{props.source}</h3>
+                    <div className="flex items-center space-x-3 bg-blue-100 border-b border-blue-200 px-3 py-1">
+                        <p className="source text-gray-900 text-sm">{props.source}</p>
                     </div>
                     <p
-                        className={`animate__animated animate__faster ${visibility} whitespace-normal mt-1 text-green-400 text-sm truncate`}
+                        className={`animate__animated animate__faster ${visibility} whitespace-normal mt-1
+                        text-gray-800 text-md px-3 py-1 truncate`}
                     >
                         {props.destination}
                     </p>
