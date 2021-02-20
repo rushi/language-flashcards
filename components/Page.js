504126d259edf6eef381caec9e77cdf85b70264f
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-20 12:42:23 +0530

    Better fix

--- components/Page.js
+++ components/Page.js
@@ -15,14 +15,12 @@ const Page = ({ active, title = 'Learn Kannada', children, ...rest }) => {
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                                <script>
                     !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
                     arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
                     d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
                     insertBefore(d,q)}(window,document,'script','_gs');
 
                     _gs('GSN-822688-L');
-                    </script>
                         `,
                     }}
                 />
