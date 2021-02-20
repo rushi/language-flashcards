e6aae5495d7a39ebbd3a01e1d1faad6b401bdbf7
Rushi Vishavadia <rushi.v@gmail.com>
2021-02-20 12:36:21 +0530

    Test analytics

--- components/Page.js
+++ components/Page.js
@@ -12,6 +12,20 @@ const Page = ({ active, title = 'Learn Kannada', children, ...rest }) => {
                 <link rel="shortcut icon" href="/favicon.png" />
                 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                <script>
+                    !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+                    arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+                    d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+                    insertBefore(d,q)}(window,document,'script','_gs');
+
+                    _gs('GSN-822688-L');
+                    </script>
+                        `,
+                    }}
+                />
             </Head>
 
             <Header />
